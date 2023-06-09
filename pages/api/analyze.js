16e0f815a2456b12e7892e823984747367e3f77b
rocketsloth <kbbb2003@gmail.com>
2023-06-09 12:05:25 -0500

    a

--- pages/api/analyze.js
+++ pages/api/analyze.js
@@ -6,13 +6,14 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function getCompletion(filteredTests) {
-  const prompt = `I have the following lab test results: ${filteredTests}. Act as a doctor and provide analysis and recommendations based on the given blood test results. analyze all the lab results and provide me with a summary and interpretation of what all the lab results mean as well as any concerning trends or potentially dangerous results. Then make some recommendations on how I can change the trending patterns for the better. Also, if any potentially dangerous levels or trends are detected, provide me with some instructions on how I can talk to my doctor about my concerns.`;
+  const prompt = `I have the following lab test results: ${filteredTests}. Act as a doctor and provide analysis and recommendations based on the given blood test results. analyze all the lab results and provide me with an interpretation of what all the lab results mean as well as any concerning trends or potentially dangerous results. Then make some recommendations on how I can change the trending patterns for the better. Also, if any potentially dangerous levels or trends are detected, provide me with some instructions on how I can talk to my doctor about my concerns.`;
 
   try {
     const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
+      model: 'gpt-4',
       prompt: prompt,
-      max_tokens: 1000,
+      max_tokens: 250,
+      top_p: 0.1,
     });
     console.log(completion.data.choices[0].text);
     return completion;
